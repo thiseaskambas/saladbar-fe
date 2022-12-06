9db77bdf5423b06b5c64377ead68dd92cb88437f
thiseaskambas <tkambas@yahoo.com>
2022-12-06 19:01:50 -0400

    css conficts still here

--- src/pages/CartsDash.tsx
+++ src/pages/CartsDash.tsx
@@ -1,6 +1,18 @@
 import React, { useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 
+import 'rsuite/dist/rsuite.min.css';
+import { DateRangePicker, InputPicker } from 'rsuite';
+
+import {
+  startOfDay,
+  endOfDay,
+  addDays,
+  subDays,
+  startOfMonth,
+  lastDayOfMonth,
+} from 'date-fns';
+import { RangeType } from 'rsuite/esm/DateRangePicker';
 import CartsTable from '../components/CartsTable';
 import { useInitializeData } from '../hooks/useInititalizeData';
 import { usePagination } from '../hooks/usePagination';
@@ -9,26 +21,51 @@ import { RootState } from '../store/store';
 import { Pagination } from './Pagination';
 import { StyledSharedMain } from './styles/shared.styles';
 
-const CartsDash = () => {
-  const date1 = new Date();
-  const date2 = new Date();
-  const dayStart = new Date(date1.setUTCHours(0, 0, 0, 0));
-  const dayEnd = new Date(date2.setUTCHours(23, 59, 59, 999));
+const RANGES: RangeType[] = [
+  {
+    label: 'today',
+    value: [startOfDay(new Date()), endOfDay(new Date())],
+    closeOverlay: false,
+  },
+  {
+    label: 'yesterday',
+    value: [
+      startOfDay(addDays(new Date(), -1)),
+      endOfDay(addDays(new Date(), -1)),
+    ],
+    closeOverlay: false,
+  },
+  {
+    label: 'last7Days',
+    value: [startOfDay(subDays(new Date(), 6)), endOfDay(new Date())],
+    closeOverlay: false,
+  },
+  {
+    label: 'This Month',
+    value: [startOfMonth(new Date()), endOfDay(lastDayOfMonth(new Date()))],
+    closeOverlay: false,
+  },
+];
 
+const data = [10, 20, 30].map((item) => ({ label: item, value: item }));
+
+const CartsDash = () => {
   const cartsState = useSelector((state: RootState) => state.carts);
   const [currentPage, setCurrentPage] = useState(1);
-  const [pageSizeLimit, _setLimit] = useState(25);
-  const [afterDate, _setAfterDate] = useState<Date | null>(dayStart);
-  const [beforeDate, _setBeforeDate] = useState<Date | null>(dayEnd);
+  const [pageSizeLimit, setLimit] = useState(10);
+  const [dateRange, setDateRange] = useState<[Date | null, Date | null]>([
+    null,
+    null,
+  ]);
 
   const options = useMemo(() => {
     return {
       page: currentPage - 1,
       limit: pageSizeLimit,
-      after: afterDate,
-      before: beforeDate,
+      after: dateRange[0],
+      before: dateRange[1],
     };
-  }, [currentPage, pageSizeLimit, beforeDate, afterDate]);
+  }, [currentPage, pageSizeLimit, dateRange[0], dateRange[1]]);
 
   useInitializeData(initializeCarts, options, cartsState.status);
 
@@ -38,8 +75,37 @@ const CartsDash = () => {
     totalItems: cartsState.totalCarts,
   });
 
+  const dateSelectHandler = (range: [Date, Date]) => {
+    const afterDate = new Date(range[0] + ' UTC');
+    const beforeDate = new Date(range[1] + ' UTC');
+    setDateRange([afterDate, beforeDate]);
+  };
+
   return (
     <StyledSharedMain>
+      <div>
+        <label>Carts to display : </label>
+        <InputPicker
+          data={data}
+          onChange={(value: string) => {
+            setLimit(Number(value)), setCurrentPage(1);
+          }}
+          cleanable={false}
+          defaultValue="10"
+          placeholder="10"
+          style={{ width: '5rem' }}
+        />
+        <DateRangePicker
+          style={{ width: 230 }}
+          onOk={(value: [Date, Date]) => dateSelectHandler(value)}
+          placeholder="click to select dates"
+          preventOverflow={true}
+          showWeekNumbers
+          showOneCalendar={false}
+          ranges={RANGES}
+        />
+      </div>
+
       {cartsState.status === 'succeeded' && (
         <CartsTable carts={cartsState.carts} />
       )}
