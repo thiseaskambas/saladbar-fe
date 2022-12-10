c957ca994e22d955a007f28037b16910c963ff01
thiseaskambas <tkambas@yahoo.com>
2022-12-09 21:39:12 -0400

    testing animations

--- src/pages/Today.tsx
+++ src/pages/Today.tsx
@@ -1,8 +1,13 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+
+import useMeasure from 'react-use-measure';
+import { useSpring, animated } from '@react-spring/web';
+
 import { getCartsForStats, initializeCarts } from '../store/carts.slice';
 import { RootState, useAppDispatch } from '../store/store';
 import functionHelpers from '../utils/functionHelpers';
+import { StyledBarCtnDiv } from './styles/today.styles';
 
 const Today = () => {
   const cartsState = useSelector((state: RootState) => state.carts);
@@ -57,19 +62,58 @@ const Today = () => {
 
   const weekday = functionHelpers.getUTCDayName();
 
+  const [ref, { width }] = useMeasure();
+
+  const Counter = ({ n }: { n: number; width?: number }) => {
+    const { number } = useSpring({
+      from: { number: 0 },
+      number: n,
+      delay: 200,
+      config: { mass: 1, tension: 20, friction: 10 },
+    });
+    return <animated.span>{number.to((n) => n.toFixed(0))}</animated.span>;
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const CounterDiv = ({ widthRef, n }: { n?: number; widthRef?: any }) => {
+    console.log({ widthRef, n });
+    const { number } = useSpring({
+      from: { number: 0 },
+      number: n,
+      delay: 1000,
+      config: { mass: 1, tension: 20, friction: 10 },
+    });
+    const { widthSpr } = useSpring({
+      from: { widthSpr: 0 },
+      widthSpr: widthRef,
+      delay: 1000,
+      config: { mass: 1, tension: 20, friction: 10 },
+    });
+
+    return (
+      <animated.div className={'fill'} style={{ width: widthSpr }}>
+        <animated.div>{number.to((n) => n.toFixed(0))}</animated.div>
+      </animated.div>
+    );
+  };
+
   return (
     <main>
       <div>Hello {userState.user.username} !</div>
       <div>
-        Today {totalProductsSoldToday} products were sold to{' '}
-        {cartsState.totalCarts} customer{cartsState.totalCarts > 1 && 's'}.
-        Total revenue is €{totalRevenueToday}
+        Today <Counter n={totalProductsSoldToday} /> products were sold to{' '}
+        <Counter n={cartsState.totalCarts} /> customer
+        {cartsState.totalCarts > 1 && 's'}. Total revenue is €
+        <Counter n={totalRevenueToday} />
       </div>
+      <StyledBarCtnDiv ref={ref}>
+        <CounterDiv n={totalRevenueToday} widthRef={width} />
+      </StyledBarCtnDiv>
       <div>
-        On last {weekday}, {totalProductsLastWeekday} products were sold to{' '}
-        {cartsState.tempTotalCarts} customer
+        On last {weekday}, <Counter n={totalProductsLastWeekday} /> products
+        were sold to <Counter n={cartsState.tempTotalCarts} /> customer
         {cartsState.tempTotalCarts > 1 && 's'}. Total revenue was €
-        {totalRevenueLastWeekday}
+        <Counter n={totalRevenueLastWeekday} />
       </div>
     </main>
   );
---
+++ src/pages/styles/today.styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const StyledMain = styled.main``;
+
+export const StyledBarCtnDiv = styled.div`
+  /* display: flex; */
+  /* flex-direction: column; */
+  /* align-items: flex-start; */
+  width: 100%;
+  background-color: lightgray;
+  & .fill {
+    background-color: pink;
+  }
+`;
