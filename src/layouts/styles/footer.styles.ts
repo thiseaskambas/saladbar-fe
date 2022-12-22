2999774ccb7ff214ecc0aba299ddcba308135bd9
thiseaskambas <tkambas@yahoo.com>
2022-12-22 17:06:55 -0400

    footer

--- src/layouts/styles/footer.styles.ts
+++ src/layouts/styles/footer.styles.ts
@@ -3,4 +3,8 @@ import styled from 'styled-components';
 export const StyledFooter = styled.footer`
   background-color: ${({ theme }) => theme.colors.mediumBrown};
   min-height: 4rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
 `;
