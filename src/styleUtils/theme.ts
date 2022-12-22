028766427e033a6b8735827cdeb9b7acec59fe69
thiseaskambas <tkambas@yahoo.com>
2022-12-22 18:07:11 -0400

    typed styled theme

--- src/styleUtils/theme.ts
+++ src/styleUtils/theme.ts
@@ -1,6 +1,21 @@
+import 'styled-components';
 import { DefaultTheme } from 'styled-components';
 
-enum colors {
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: typeof colors;
+    backDropShadow: typeof backDropShadow;
+    neobrutalColors: typeof neobrutalColors;
+    paleBrutalColors: typeof paleBrutalColors;
+  }
+}
+
+enum backDropShadow {
+  webkitboxShadow = '0px 3px 8px 0px rgba(66, 66, 66, 0.64)',
+  mozzilaboxShadow = '0px 3px 8px 0px rgba(66, 66, 66, 0.64)',
+  boxShadow = '0px 3px 8px 0px rgba(66, 66, 66, 0.64)',
+}
+enum paleBrutalColors {
   PINK = '#F1C2CC',
   PURPLE = '#C9BFFE',
   ORANGE = '#FEE2C7',
@@ -17,32 +32,30 @@ enum neobrutalColors {
   DARKGRAY = '#01416',
   WHITE = '#FFFFFF',
 }
+
+enum colors {
+  blue = '#3D5A80',
+  skyBlue = '#cae8fa',
+  lightBlue = '#98C1D9',
+  orange = '#F7882F',
+  lightOrange = '#F7B681',
+  yellow = '#F7C331',
+  lightYellow = '#FDE08E',
+  darkBrown = '#DCC7AA',
+  mediumBrown = '#E2CEBC',
+  lightBrown = '#ede3da',
+  white = '#FAFAFF',
+  lightGray = '#ebeded',
+  gray = '#7d888d',
+  newBlue = '#3d91ff',
+  lightGreen = '#90EE90',
+}
+
 const theme: DefaultTheme = {
-  colors: {
-    blue: '#3D5A80',
-    skyBlue: '#cae8fa',
-    lightBlue: '#98C1D9',
-    orange: '#F7882F',
-    lightOrange: '#F7B681',
-    yellow: '#F7C331',
-    lightYellow: '#FDE08E',
-    darkBrown: '#DCC7AA',
-    mediumBrown: '#E2CEBC',
-    lightBrown: '#ede3da',
-    white: '#FAFAFF',
-    lightGray: '#ebeded',
-    gray: '#7d888d',
-    newBlue: '#3d91ff',
-    lightGreen: '#90EE90',
-  },
-  error: {
-    color: 'darkGray',
-  },
-  backDropShadow: {
-    webkitboxShadow: '0px 3px 8px 0px rgba(66, 66, 66, 0.64)',
-    mozzilaboxShadow: '0px 3px 8px 0px rgba(66, 66, 66, 0.64)',
-    boxShadow: '0px 3px 8px 0px rgba(66, 66, 66, 0.64)',
-  },
+  neobrutalColors,
+  colors,
+  backDropShadow,
+  paleBrutalColors,
 };
 
 export default theme;
